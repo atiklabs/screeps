763df9bc43c7b1d3b496a9dc7da836f358584986
Atik <atiklabs@pm.me>
2016-10-27 10:39:33 +0300

    Use prototype for spawn.

--- prototype.structureSpawn.js
+++ prototype.structureSpawn.js
@@ -21,7 +21,7 @@ module.exports = function () {
         if (name !== null && isNaN(name)) {
             Game.creeps[name].memory.role = 'worker';
             Game.creeps[name].memory.state = 'init';
-            Game.creeps[name].memory.initial_room = this.roomName;
+            Game.creeps[name].memory.initial_room = this.room.roomName;
             Game.creeps[name].memory.source_index = null;
             Game.creeps[name].memory.level = level;
             return name;
