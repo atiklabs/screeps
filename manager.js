ddc3375573fcc2c4940a6ce4d004469cab33bd3c
Atik <atiklabs@pm.me>
2016-10-31 22:18:00 +0200

    Many rooms.

--- manager.js
+++ manager.js
@@ -55,7 +55,7 @@ var manager = {
             }
             if (typeof room.controller != 'undefined') {
                 var currentRoomWorkers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker' && creep.memory.home == roomName);
-                if (currentRoomWorkers.length == 0) {
+                if (currentRoomWorkers.length < 2) {
                     var allWorkers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker' && creep.ticksToLive > 1000);
                     allWorkers[0].setValue('home', roomName);
                 }
