0ca5cc13f01bb83e007aaea51cfbac44674dd436
Atik <atiklabs@pm.me>
2016-11-05 09:32:20 +0200

    Time to attack kov.

--- prototype.structureTower.js
+++ prototype.structureTower.js
@@ -6,7 +6,7 @@ module.exports = function () {
      * Defend room
      */
     StructureTower.prototype.defendRoom = function () {
-        var hostiles = this.room.find(FIND_HOSTILE_CREEPS);
+        var hostiles = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (hostiles.length > 0) {
             this.attack(hostiles[0]);
         }
